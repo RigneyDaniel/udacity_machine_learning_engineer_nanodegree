edda159c543b983edb053c1507907b5365b30873
unknown <daniel.rigney98@gmail.com>
2020-12-29 09:26:24 +1000

    update and redeploy on aws

--- sagemaker-deployment/Project/serve/predict.py
+++ sagemaker-deployment/Project/serve/predict.py
@@ -69,9 +69,6 @@ def predict_fn(input_data, model):
     #       You should produce two variables:
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
-
-    data_X = None
-    data_len = None
     
     data_words = review_to_words(input_data)
     data_X, data_len = convert_and_pad_data(model.word_dict, data_words)
@@ -89,10 +86,10 @@ def predict_fn(input_data, model):
 
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
-
-    result = None
     
-    predict = model(data)
-    result = round(predict)
+    predict = model(data).detach()
+    predict = predict.numpy()
+    result = np.round(predict)
+    result  = result.astype(np.int)
 
     return result
