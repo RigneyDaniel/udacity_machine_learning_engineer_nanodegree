bd6122bd809ca87698c726ab6a26bb155778eed5
unknown <daniel.rigney98@gmail.com>
2020-12-29 19:30:15 +1000

    changes made locally

--- sagemaker-deployment/Project/serve/predict.py
+++ sagemaker-deployment/Project/serve/predict.py
@@ -58,7 +58,7 @@ def output_fn(prediction_output, accept):
     return str(prediction_output)
 
 def predict_fn(input_data, model):
-   print('Inferring sentiment of input data.')
+    print('Inferring sentiment of input data.')
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     
@@ -71,7 +71,7 @@ def predict_fn(input_data, model):
     #         data_len - The length of the review
     
     data_words = review_to_words(input_data)
-    data_X, data_len = convert_and_pad_data(model.word_dict, data_words)
+    data_X, data_len = convert_and_pad(model.word_dict, data_words)
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
